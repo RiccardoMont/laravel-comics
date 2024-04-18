7b9d7cd24bfcc2fd9c43e16b6a88418fd0aaa046
RiccardoMont <riccardomontalto94@gmail.com>
2024-04-18 18:22:18 +0200

    working on green price bar

--- resources/views/guests/action-comics.blade.php
+++ resources/views/guests/action-comics.blade.php
@@ -8,20 +8,56 @@
         <div class="comic-selected">
             <img class="resizeComic" src="{{$comics[0]['thumb']}}" alt="">
         </div>
-
         <div class="row">
-
-            <div class="comicCard">
-                <div class="maskComic">
-                    <img class="resizeComic" src="" alt="">
-                </div>
-                <h4>4</h4>
+            <div class="texts">
+                <h2>{{$comics[0]['title']}}</h2>
+                <div class="price-quantity"></div>
+                <p>{{$comics[0]['description']}}</p>
             </div>
-
-        </div>
-        <div class="loadMore">
-            <button>LOAD MORE</button>
+            <div class="adv">
+                <h4>ADVERTISEMENT</h4>
+                <img src="{{Vite::asset('resources/images/adv.jpg')}}" alt="">
+            </div>
         </div>
     </div>
+</div>
+<div class="content-action-comics-info">
+    <div class="container">
+        <div class="row">
+            <div class="talent">
+                <h2>Talent</h2>
+                <table>
+                    <tr>
+                        <th>Art by:</th>
+                        <td>Marco Rossi, Gino Giallo, Paolo Viola</td>
+                    </tr>
+                    <tr>
+                        <th>Written by:</th>
+                        <td>Chicco Verde, Simone Arancione, Rosa Rossa</td>
+                    </tr>
+                </table>
+            </div>
+            <div class="specs">
+                <h2>Specs</h2>
+                <table>
+                    <tr>
+                        <th>Series:</th>
+                        <td>{{$comics[0]['series']}}</td>
+                    </tr>
+                    <tr>
+                        <th>U.S. Price:</th>
+                        <td>{{$comics[0]['price']}}</td>
+                    </tr>
+                    <tr>
+                        <th>On Sale Date</th>
+                        <td>{{$comics[0]['sale_date']}}</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+    
+    
+</div>
 
-    @endsection
+@endsection
